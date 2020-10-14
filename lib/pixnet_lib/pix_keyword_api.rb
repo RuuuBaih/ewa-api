c6ca1b5baea12000d6e8629d8ce308fa0f1d53f9
rainyjonne <rainyjonne@gmail.com>
2020-10-14 20:57:49 +0800

    Api files and one tiny test

---
+++ lib/pixnet_lib/pix_keyword_api.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'http'
+require_relative 'keywordlists'
+
+# Food & Restaurant reviews and articles integrated application
+module JustRuIt
+  module Errors
+    class NotFound < StandardError; end
+    class Unauthorized < StandardError; end
+  end
+
+  HTTP_ERROR = {
+    401 => Errors::Unauthorized,
+    404 => Errors::NotFound
+  }.freeze
+
+  def call_pix_url(url)
+    result = HTTP.get(url)
+    # below puts is for testing
+    puts result
+    successful?(result) ? result : raise(HTTP_ERROR[result.code])
+  end
+
+  def successful?(result)
+    HTTP_ERROR.keys.include?(result.code) ? false : true
+  end
+
+  # Library for Pixnet keyword find keyword API
+  class PixKeywordApi
+    API_PROJECT_ROOT = 'https://emma.pixnet.cc/explorer/keywords?format=json&key='
+
+    def initialize(keyword)
+      @keyword = keyword
+    end
+
+    def keyword_lists
+      keyword_req_url = pix_keyword_api_path(@keyword)
+      # below puts is for testing
+      puts keyword_req_url
+      related_keywords = call_pix_url(keyword_req_url).parse
+      KeywordLists.new(related_keywords).keyword_lists
+    end
+
+    private
+
+    def pix_keyword_api_path(path)
+      "#{API_PROJECT_ROOT}#{path}"
+    end
+  end
+end
