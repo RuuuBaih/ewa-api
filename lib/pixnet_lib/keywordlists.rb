e9b0b27a0160e22474f83b9220d353afafafeaed
rainyjonne <rainyjonne@gmail.com>
2020-10-18 20:37:44 +0800

    Modify keywordlists file

--- lib/pixnet_lib/keywordlists.rb
+++ lib/pixnet_lib/keywordlists.rb
@@ -2,7 +2,6 @@
 
 # Food & Restaurant reviews and articles integrated application
 module JustRuIt
-<<<<<<< HEAD
   # Provide accesss to keyword lists data
   class KeywordLists
     def initialize(keyword_list_data)
@@ -14,16 +13,3 @@ module JustRuIt
     end
   end
 end
-=======
-    # Provide accesss to keyword lists data
-    class KeywordLists
-      def initialize(keyword_list_data)
-        @keyword_lists = keyword_list_data
-      end
-  
-      def keyword_lists
-        @keyword_lists['data']
-      end
-    end
-end
->>>>>>> spec
