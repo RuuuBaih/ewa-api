bd9e38b6b1903adf7628b11f84a736388c3c86bd
RonaLu <rona26lu@gmail.com>
2020-10-23 23:59:19 +0800

    refactor keyword api

--- lib/pixnet_lib/poi/pix_poi_api.rb
+++ lib/pixnet_lib/poi/pix_poi_api.rb
@@ -7,21 +7,7 @@ require_relative 'poilists'
 module JustRuIt
   # Library for Pixnet poi list API
   class PixPoiApi
-    API_PROJECT_ROOT = 'https://emma.pixnet.cc/poi?page=1&per_page='
-
-    module Errors
-      class NotFound < StandardError; end
-      class Unauthorized < StandardError; end
-    end
-
-    HTTP_ERROR = {
-      401 => Errors::Unauthorized,
-      404 => Errors::NotFound
-    }.freeze
-
-    def successful?(result)
-      HTTP_ERROR.keys.include?(result.code) ? false : true
-    end
+    POI_API_PATH = 'https://emma.pixnet.cc/poi?page=1&per_page='
 
     def initialize(per_page)
       @per_page = per_page
@@ -47,6 +33,42 @@ module JustRuIt
       # puts result
       successful?(result) ? result : raise(HTTP_ERROR[result.code])
     end
+    class Request
+      def initialize(resource_root, keyword)
+        @resource_root = resource_root
+        @keyword = keyword
+      end
 
+      def keyword_http
+        get(@resource_root + @keyword)
+      end
+
+      def get(url)
+        http_response = HTTP.get(url)
+
+        Response.new(http_response).tap do |response|
+          raise(response.error) unless response.successful?
+        end
+      end
+    end
+
+    # Decorates HTTP responses from Github with success/error
+    class Response < SimpleDelegator
+      Unauthorized = Class.new(StandardError)
+      NotFound = Class.new(StandardError)
+
+      HTTP_ERROR = {
+        401 => Unauthorized,
+        404 => NotFound
+      }.freeze
+
+      def successful?
+        HTTP_ERROR.keys.include?(code) ? false : true
+      end
+
+      def error
+        HTTP_ERROR[code]
+      end
+    end
   end
 end
