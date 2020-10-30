df8f6a0c87d92c543264107f159fd2b5575038e6
rainyjonne <rainyjonne@gmail.com>
2020-10-31 01:42:58 +0800

    Add entities (needs reek, rubocop & flog modification)

--- lib/entities/restaurant.rb
+++ lib/entities/restaurant.rb
@@ -1,27 +1,31 @@
 # frozen_string_literal: true
 
+require 'dry-types'
+require 'dry-struct'
+
 module Ewa
   module Entity
     # Restaurant Entity
     class Restaurant < Dry::Struct
-      include Dry::Types.module
+      include Dry.Types
 
-      attribute :restaurant_id, Integer.required
-      attribute :name,          Strict::String.required
+      attribute :restaurant_id, Integer.optional
+      attribute :name,          Strict::String
       attribute :town,          Strict::String.optional
       attribute :money,         Strict::Integer.optional
       attribute :city,          Strict::String.optional
       attribute :telephone,     Strict::String.optional
       attribute :cover_img,     Strict::String.optional
       attribute :tags,          Strict::Array.of(String).optional
-      attribute :pixnet_rating, Strict::Integer.optional
-      attribute :google_rating, Strict::Ingeter.optional
+      attribute :pixnet_rating, Strict::Float.optional
+      attribute :google_rating, Strict::Float.optional
       attribute :open_hours,    Strict::Array.of(String).optional
-      attribute :reviews,       Strict::Array.of(Hash.schema(author_name: Strict::String.optional,
-                                                             profile_photo_url: Strict::String.optional,
-                                                             relative_time_description: Strict::String.optional,
-                                                             text: Strict::String.optional,
-                                                             language: Strict::String.optional))
+      attribute :reviews,       Strict::Array.of(Hash.schema(
+                                                   author_name: Strict::String.optional,
+                                                   profile_photo_url: Strict::String.optional,
+                                                   relative_time_description: Strict::String.optional,
+                                                   text: Strict::String.optional
+                                                 ))
     end
   end
 end
