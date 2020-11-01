88a3392eac0ffa74840a10051870a97e0f8d111d
RonaLu <rona26lu@gmail.com>
2020-11-01 14:54:32 +0800

    test poi hash

--- spec/test.rb
+++ spec/test.rb
@@ -7,9 +7,8 @@ CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 GMAP_TOKEN = CONFIG['gmap_token']
 poi = Ewa::Restaurant::RestaurantMapper.new(GMAP_TOKEN).poi_details
 puts poi
-place_name = poi[0]
-puts place_name
-#data = Ewa::Restaurant::RestaurantMapper.new(GMAP_TOKEN).gmap_place_details(poi)
+poi_hash = Ewa::Restaurant::RestaurantMapper.new(GMAP_TOKEN).restaurant_obj_lists
+data = Ewa::Restaurant::RestaurantMapper.new(GMAP_TOKEN).gmap_place_details(poi)
 
 
-#puts data
+puts data
