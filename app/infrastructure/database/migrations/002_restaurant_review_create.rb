9d2909501212427db8238bece4c789755e1951bb
RonaLu <rona26lu@gmail.com>
2020-11-05 17:36:45 +0800

    add review migration

--- app/infrastructure/database/migrations/002_restaurant_review_create.rb
+++ app/infrastructure/database/migrations/002_restaurant_review_create.rb
@@ -5,10 +5,12 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:reviews) do
-      primary_key :article_id
-      # 等阿嚕弄完之後要加foreign key
-      String :restaurant_name, unique: true
-      String :link
+      primary_key :review_id
+      
+      String :author_name
+      String :profile_photo_url
+      String :relative_time_description
+      String :text
 
       DateTime :created_at
       DateTime :updated_at
