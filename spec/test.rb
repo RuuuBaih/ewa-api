c15e03da850c203ce4938cefe39e8587873bbeb9
RonaLu <rona26lu@gmail.com>
2020-11-11 15:41:26 +0800

    modify poi

--- spec/test.rb
+++ spec/test.rb
@@ -12,6 +12,13 @@ puts @article_mapper.new(REST_NAME).the_newest_article
 
 #puts 'poi_details'
 poi = @restaurant_mapper.new('GMAP_TOKEN').poi_details
+puts poi
+puts poi.length
+puts poi.class
+puts poi[0]['category_id']
+
+# puts @restaurant_mapper.new(GMAP_TOKEN).gmap_place_details(poi)[place_id]
+=begin
 puts 'restaurant_list'
 #puts @restaurant_mapper.new(GMAP_TOKEN).restaurant_obj_lists[0][:reviews]
 #puts @restaurant_mapper.new(GMAP_TOKEN).gmap_place_details(poi)[place_id]
@@ -20,3 +27,4 @@ GMAP_API_PATH = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/js
 
 @gateway_classes = Ewa::Gmap::PlaceApi
 puts @gateway_classes.new('GMAP_TOKEN', place_name).place_id['candidates'].empty?
+=end
