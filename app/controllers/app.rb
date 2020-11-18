97e31364002c5a8e32fce3599980b3d5b45f25fb
RonaLu <rona26lu@gmail.com>
2020-11-18 22:34:13 +0800

    add cookies

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -9,6 +9,7 @@ module Ewa
     plugin :render, engine: 'slim', views: 'app/views'
     plugin :assets, css: 'style.css', path: 'app/views/assets'
     plugin :halt
+    plugin :flash
 
     route do |routing|
       routing.assets # load CSS
@@ -34,19 +35,22 @@ module Ewa
             # pick 9 restaurants
             rests = Mapper::RestaurantOptions.new(selected_entities)
             pick_9rests = rests.random_9picks
+            session[:rests] = rests
+            session[:pick_9rests] = pick_9rests
             # pick_one = @rests.pick_one(@pick_9rests, 2)
-            #view 'restaurant', locals: { pick_9rests: pick_9rests }
-            routing.redirect "restaurant/test_detail"
+            view 'restaurant', locals: { pick_9rests: pick_9rests }
+            # routing.redirect "restaurant/test_detail"
           end
         end
-        routing.on 'test_detail' do
-          # GET /restaurant/test_detail
-          # select one of them
-          routing.is do
-            # num = routing.params['num']
-            pick_one = rests.pick_one(pick_9rests, 2)
-            view 'test_detail', locals: { pick_one: pick_one }
-          end
+      end
+
+      routing.on 'test_detail' do
+        # GET /restaurant/test_detail
+        # select one of them
+        routing.is do
+          # num = routing.params['num']
+          pick_one = session[:rests].pick_one(session[:pick_9rests], 2)
+          view 'test_detail', locals: { pick_one: pick_one }
         end
       end
     end
