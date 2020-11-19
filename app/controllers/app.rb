e1f55e624b6956e4182296dcf61db94cf6c85d60
RonaLu <rona26lu@gmail.com>
2020-11-19 17:46:33 +0800

    edit controller

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -14,28 +14,29 @@ module Ewa
     route do |routing|
       routing.assets # load CSS
 
-      # GET /
+      # POST /
       routing.root do
         restaurants = Repository::For.klass(Entity::Restaurant).all
+        session[:pick_9rests] ||= []
         view 'home_test', locals: { restaurants: restaurants }
       end
 
       routing.on 'restaurant' do
         routing.is do
-          # GET /restaurant
+          # POST /restaurant
           routing.post do
             # Get restaurant information from pixnet & gmap api
-                         #restaurant_entities = Restaurant::RestaurantMapper.new(App.config.GMAP_TOKEN).restaurant_obj_lists
+=begin            
+                         restaurant_entities = Restaurant::RestaurantMapper.new(App.config.GMAP_TOKEN).restaurant_obj_lists
             
-                         #restaurant_repo_entities = restaurant_entities.map do |restaurant_entity|
-                           #Repository::For.entity(restaurant_entity).create(restaurant_entity)
-                         #end
+                         restaurant_entities.map do |restaurant_entity|
+                           Repository::For.entity(restaurant_entity).create(restaurant_entity)
+                         end
+=end                         
             # parameters call from view
             town = routing.params['town']
             min_money = routing.params['min_money']
             max_money = routing.params['max_money']
-            img_num = routing.params['img_num']
-
             # select restaurants from the database
             selected_entities = Repository::For.klass(Entity::Restaurant)
                                                .find_by_town_money(town, min_money, max_money)
@@ -43,8 +44,8 @@ module Ewa
             # pick 9 restaurants
             rests = Mapper::RestaurantOptions.new(selected_entities)
             pick_9rests = rests.random_9picks
-            session[:rests] = rests
             session[:pick_9rests] = pick_9rests
+            #session[:img_num] = img_num
             # pick_one = @rests.pick_one(@pick_9rests, 2)
             view 'restaurant', locals: { pick_9rests: pick_9rests }
             # routing.redirect "restaurant/test_detail"
@@ -52,13 +53,15 @@ module Ewa
         end
       end
 
-      routing.on 'test_detail' do
-        # GET /restaurant/test_detail
+      routing.on 'pick' do
+        # POST /restaurant/test_detail
         # select one of them
         routing.is do
-          # num = routing.params['num']
-          pick_one = session[:rests].pick_one(session[:pick_9rests], 2)
-          view 'test_detail', locals: { pick_one: pick_one }
+          routing.post do
+            img_num = routing.params['img_num']
+            pick_nine = session[:pick_9rests]
+            view 'test_detail', locals: { pick_nine: pick_nine, img_num: img_num }
+          end
         end
       end
     end
