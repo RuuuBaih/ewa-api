50cf669f6a2d29f752d9802401562a29af3da1a9
RonaLu <rona26lu@gmail.com>
2020-11-29 21:23:32 +0800

    history controller

--- app/application/restaurant_others/history.rb
+++ app/application/restaurant_others/history.rb
@@ -5,14 +5,25 @@ require 'dry/transaction'
 module Ewa
   module RestaurantOthers
     # Retrieves restaurant entity by searching restaurant name
-    class History
+    class History_id
       include Dry::Transaction
-
       def call(history_records)
-        history = history_records.map do |history_id|
-          Repository::For.klass(Entity::Restaurant).find_by_rest_id(history_id)
+        history_ID = history_records.map do |history_id|
+          Repository::For.klass(Entity::Restaurant).find_by_rest_id(history_id).id
         end
-        Success(history)
+        Success(history_ID)
+        rescue StandardError
+          Failure('資料錯誤 Database error!')
+      end
+    end
+
+    class History_name
+      include Dry::Transaction
+      def call(history_records)
+        history_name = history_records.map do |history_id|
+          Repository::For.klass(Entity::Restaurant).find_by_rest_id(history_id).name
+        end
+        Success(history_name)
         rescue StandardError
           Failure('資料錯誤 Database error!')
       end
