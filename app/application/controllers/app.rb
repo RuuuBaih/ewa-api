e3095400eae7a71dd5f74fc8ae91633c08e751dc
RonaLu <rona26lu@gmail.com>
2020-11-29 20:41:19 +0800

    refactor controller function actions

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -26,7 +26,13 @@ module Ewa
         # Get cookie viewer's previously seen projects
         session[:watching] ||= []
 
-        restaurants = Repository::For.klass(Entity::Restaurant).all
+        rest_all = RestaurantActions::Rest.new.RestAll
+
+        if rest_all.failure?
+          flash[:error] = rest_all.failure
+        else
+          restaurants = rest_all.value!
+        end
 
         if session[:watching].count > 5
           session[:watching] = session[:watching][0..4]
@@ -42,16 +48,6 @@ module Ewa
             flash.now[:notice] = '尋找城市，開啟饗宴！ Search a place to get started!'
           end
         end
-=begin
-        unless session[:watching].nil?
-          history = session[:watching].map do |history_id|
-            Repository::For.klass(Entity::Restaurant).find_by_rest_id(history_id)
-          end
-        else
-          flash.now[:notice] = '尋找城市，開啟饗宴！ Search a place to get started!'
-        end
-=end
-        # session[:pick_9rests] ||= []
         view 'home_test', locals: { restaurants: restaurants, history: history }
       end
 
@@ -66,34 +62,36 @@ module Ewa
             if (min_money.to_i >= max_money.to_i) ||
                   (min_money.to_i < 0) || (max_money.to_i <= 0)
               flash[:error] = '輸入格式錯誤 Wrong number type.'
-              response.status = 400
               routing.redirect '/'
             end
             if (max_money.to_i <= 100)
               flash[:error] = '金額過小 Max price is too small.'
-              response.status = 400
               routing.redirect '/'
             end
             # select restaurants from the database
-            selected_entities = Repository::For.klass(Entity::Restaurant)
-                                               .find_by_town_money(town, min_money, max_money)
+            selected_rest = RestaurantActions::SelectRest.new.call(town, min_money, max_money)
+            if selected_rest.failure?
+              flash[:error] = selected_rest.failure
+            else
+              selected_entities = selected_rest.value!
+            end
 
             # pick 9 restaurants
-            rests = Mapper::RestaurantOptions.new(selected_entities)
-            pick_9rests = rests.random_9picks
-            rests_info = Mapper::RestaurantOptions::GetRestInfo.new(pick_9rests)
-            # rests_info = Restaurant::RestaurantOptionsMapper::GetRestInfo.new(pick_9rests)
+            rests = RestaurantActions::Pick_9.new.call(selected_entities)
+            if rests.failure?
+              flash[:error] = rests.failure
+            else
+              rests_info = rests.value!
+            end
             pick_ids = rests_info._9_id_infos
-            img_links = rests_info.random_thumbs
-            pick_names = rests_info._9_name_infos
-            session[:pick_9rests] = pick_ids
             if pick_ids.count < 9
               flash[:error] = '資料過少，無法顯示 Not enough data.'
               response.status = 400
               routing.redirect '/'
             end
-            # session[:img_num] = img_num
-            # pick_one = @rests.pick_one(@pick_9rests, 2)
+            img_links = rests_info.random_thumbs
+            pick_names = rests_info._9_name_infos
+            session[:pick_9rests] = pick_ids
             view 'restaurant', locals: { pick_9rests: pick_ids, img_links: img_links, pick_names: pick_names }
           end
         end
@@ -122,7 +120,12 @@ module Ewa
 
           routing.on String do |rest_id|
             routing.get do
-              rest_detail = Repository::For.klass(Entity::Restaurant).find_by_rest_id(rest_id)
+              rest_find = RestaurantActions::FindRest.new.call(rest_id)
+              if rest_find.failure?
+                flash[:error] = rest_find.failure
+              else
+                rest_detail = rest_find.value!
+              end
               session[:watching].insert(0, rest_detail.id).uniq!
               view 'res_detail', locals: { rest_detail: rest_detail }
             end
--- app/application/restaurant_actions/select_rest.rb
+++ app/application/restaurant_actions/select_rest.rb
@@ -3,18 +3,29 @@
 require 'dry/transaction'
 
 module Ewa
-  module RestaurantOthers
-    # Retrieves restaurant entity by searching restaurant name
-    class History
+  module RestaurantActions
+    # filter restaurants based on money 
+    class SelectRest
       include Dry::Transaction
+      def call(town, min_money, max_money)
+        selected_entities = Repository::For.klass(Entity::Restaurant)
+                            .find_by_town_money(town, min_money, max_money)
+        Success(selected_entities)
+      rescue StandardError
+        Failure('篩選資料錯誤 Filter error!')
+      end
+    end
 
-      def call(history_records)
-        history = history_records.map do |history_id|
-          Repository::For.klass(Entity::Restaurant).find_by_rest_id(history_id)
-        end
-        Success(history)
-        rescue StandardError
-          Failure('資料錯誤 Database error!')
+    # generate 9 restaurants
+    class Pick_9
+      include Dry::Transaction
+      def call(selected_entities)
+        rests = Mapper::RestaurantOptions.new(selected_entities)
+        pick_9rests = rests.random_9picks
+        rests_info = Mapper::RestaurantOptions::GetRestInfo.new(pick_9rests)
+        Success(rests_info)
+      rescue StandardError
+        Failure('篩選資料錯誤 Filter error!')
       end
     end
   end
