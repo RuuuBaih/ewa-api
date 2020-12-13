4c7b67be93223a574f539dc32d8410ec41843e31
Vivian Lu <vivianlu@Yu-Hande-MacBook-Pro.local>
2020-12-13 22:52:34 +0800

    add: api_spec

--- spec/tests_acceptance/api_spec.rb
+++ spec/tests_acceptance/api_spec.rb
@@ -4,6 +4,7 @@ require_relative '../helpers/spec_helper'
 require_relative '../helpers/vcr_helper'
 require_relative '../helpers/database_helper'
 require 'rack/test'
+require 'uri'
 
 def app
   Ewa::App
@@ -17,7 +18,7 @@ describe 'Test API routes' do
 
   before do
     VcrHelper.configure_vcr_for_restaurant
-    DatabaseHelper.wipe_database
+    #DatabaseHelper.wipe_database
   end
 
   after do
@@ -37,13 +38,46 @@ describe 'Test API routes' do
 
   describe 'display rests infos' do
     it 'should be able to return rest infos' do
-
       get "/api/v1/restaurants"
       _(last_response.status).must_equal 200
-      puts last_response.inspect
       rest_infos = JSON.parse last_response.body
-      puts rest_infos
-      _(rest_infos.count).must_equal 27
+      _(rest_infos['rests_infos'].count).must_equal 5
+    end
+
+    it '(GOOD town) should report total for valid town' do
+      url_good_town = URI::escape("/api/v1/restaurants?town=中山區")
+      get "#{url_good_town}"
+      _(last_response.status).must_equal 200
+      rest_infos = JSON.parse last_response.body
+      _(rest_infos['total'].class).must_equal Integer
+    end
+
+    it '(BAD town) should report not found for invalid town' do
+      get "/api/v1/restaurants?town=123"
+      _(last_response.status).must_equal 404
+      body = JSON.parse(last_response.body)
+      _(body['status']).must_equal 'not_found'
+    end
+  end
+
+  describe 'search by id' do
+    it 'should be able to return rest infos of id' do
+      get "/api/v1/restaurants/picks/1"
+      _(last_response.status).must_equal 200
+      rest_infos_pick_1 = JSON.parse last_response.body
+      _(rest_infos_pick_1['pick_rest']['id']).must_equal 1
+      _(rest_infos_pick_1['pick_rest']['name']).must_equal "私宅打邊爐"
+    end
+
+    describe 'search by name' do
+      it 'should be able to return rest infos of name' do
+        url_search_name = URI::escape("/api/v1/restaurants/searches?name=私宅打邊爐")
+        get "#{url_search_name}"
+        _(last_response.status).must_equal 200
+        rest_infos_name = JSON.parse last_response.body
+        _(rest_infos_name['searched_rests'][0]['id']).must_equal 1
+        _(rest_infos_name['searched_rests'][0]['name']).must_equal "私宅打邊爐"
+      end
     end
   end
 end
